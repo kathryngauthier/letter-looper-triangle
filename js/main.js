f850d21c5b2d98bdc7d392b1d5f20e36ec9cb530
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-03-03 19:53:18 -0500

    letter looper 1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var letter = prompt('Enter a letter');
+
+if (letter == 'a') {
+  document.write('a');
+  document.write('<br>');
+}
+
+for (counter = 0; counter < 10; counter++) {
+  document.write('a');
+  document.write('<br>');
+}
