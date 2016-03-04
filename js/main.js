321f288d93496093d769b108721a2bf01d65fcf7
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-03-03 19:57:03 -0500

    fixed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 var letter = prompt('Enter a letter');
 
+var n = 0;
+
 if (letter == 'a') {
   document.write('a');
-  document.write('<br>');
+  document.write('<br>') ;
 }
 
-for (counter = 0; counter < 10; counter++) {
-  document.write('a');
+for (i = 0; i < 15; i++) {
+
+    document.write(letter);
+  }
   document.write('<br>');
 }
